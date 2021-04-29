291aefb2ccaad9ffa2a5224beb37b7f23a5be077
Nicolas Nenny-Pirotte <45941538+nicolasNP@users.noreply.github.com>
2021-04-29 10:23:26 +0200

    Worked a bit on the market extract
    Not always working as intended need to check directly on scrapped website

--- Scrapping usine digitale.py
+++ Scrapping usine digitale.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[68]:
+
+
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -5,15 +11,23 @@ import re
 import time
 import datetime
 
+
+# In[69]:
+
+
 def elapsed_time(a, b):
     elapsed = round(b - a)
     res = datetime.timedelta(seconds = elapsed)
     print(res)
 
+
+# In[70]:
+
+
 start = time.time()
 link_list = []
 records = []
-usine_url = 'https://www.usine-digitale.fr'
+usine_url = 'https://www.usine-digitale.fr/annuaire-start-up/'
 try: 
     for page_number in range(0, 1000, 1):
         
@@ -30,11 +44,27 @@ try:
                     number_on_the_page += 1  
             else:
                 break
-except:
-    print("An error occured at page number {} please verify".format(page_number))
+except Exception as e:
+    print("An error occured at page number {} please verify. {}".format(page_number,print(e)))
     
 end = time.time()
 elapsed_time(start, end)
+
+
+# In[66]:
+
+
+# r = requests.get("https://www.usine-digitale.fr/annuaire-start-up/tapvalue,252906")
+# soup = BeautifulSoup(r.text, 'html.parser')
+# name = soup.find('h1', attrs = {'itemprop': 'name'}).contents[0]
+# products = soup.find('div', attrs = {'itemprop': 'makesOffer'}).p.contents[0].strip()
+# market = soup.find(text = re.compile('Marché')).next.next.contents[0].text[3:]
+# founders = soup.find('div', attrs = {'itemprop': 'founders'}).p.contents[0][5:]
+# founders
+
+
+# In[72]:
+
 
 start = time.time()
 i = 0
@@ -46,7 +76,16 @@ for links in link_list:
         soup = BeautifulSoup(r.text, 'html.parser')
         name = soup.find('h1', attrs = {'itemprop': 'name'}).contents[0]
         products = soup.find('div', attrs = {'itemprop': 'makesOffer'}).p.contents[0].strip()
-        market = soup.find_all('div', attrs = {'class': 'txtArt'})[3].p.contents[0].strip()
+        market = soup.find(text = re.compile('Marché')).next.next.contents
+        if len(market) > 1:
+            market_list = []
+            for i in range(0, len(market), 2):
+                market = soup.find(text = re.compile('Marché')).next.next.contents[i].text[3:]
+                market_list.append(market)
+            market = market_list
+        else:
+            market = soup.find(text = re.compile('Marché')).next.next.contents[0].text[3:]
+            
         founders = soup.find('div', attrs = {'itemprop': 'founders'}).p.contents
         i = 0
         if len(founders) > 1:
@@ -54,8 +93,13 @@ for links in link_list:
             for i in range(0, len(founders), 2):
                 founder = soup.find('div', attrs = {'itemprop': 'founders'}).p.contents[i].strip()
                 founders_list.append(founder)
-        location =  soup.find('div', attrs = {'itemprop': 'address'}).p.text[3:]
-
+            founders = founders_list
+        else:
+            founders = soup.find('div', attrs = {'itemprop': 'founders'}).p.contents[0][5:]
+        try:        
+            location =  soup.find('div', attrs = {'itemprop': 'address'}).p.text[3:]
+        except:
+            location = ""
         try:
             creation = soup.find('meta', itemprop='foundingDate')['content']
         except:
@@ -81,13 +125,24 @@ for links in link_list:
         except:
             phone = ""
     except Exception as e:
-        print('Erreur: {}, sur le lien {}'.format(str(e), _, links))
-    records.append((name, products, market, founders_list, location, creation, address, employees, website, mail, phone))
+        print('Erreur: {}, sur le lien {}'.format(str(e), links))
+    
+    records.append((name, products, market, founders, location, creation, address, employees, website, mail, phone))
 
 end = time.time()
 elapsed_time(start, end)
 
+
+# In[73]:
+
+
 dataframe_usine_digitale = pd.DataFrame(records, columns=['Nom', 'Produits', 'Marché', 'Créateurs', 'Implantation', 'Date de création', 'Adresse du siège', 'Nombre d\'employés', 'Site web', 'Email', 'Téléphone'])
 dataframe_usine_digitale.to_csv('TD_usine_digitale.csv', index = False, encoding = 'utf-8')
 dataframe_usine_digitale
+
+
+# In[ ]:
+
+
+
 
